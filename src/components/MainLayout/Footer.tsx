e2987081776791ff5f4fc666f06f12178eddc92d
melkuo <hi@melodykuo.com>
2025-08-13 14:36:04 -0400

    Add 'Get Involved' to nav. Update FAQ answers. Update contact email to hi@buildcanada.com. Replace Beehiiv newsletter component with link to BC substack.

--- src/components/MainLayout/Footer.tsx
+++ src/components/MainLayout/Footer.tsx
@@ -1,88 +1,11 @@
 "use client";
 
 import { ExternalLink } from "@/components/Layout";
-import { useFormState } from "@/hooks/useFormState";
 import { Trans, useLingui } from "@lingui/react/macro";
-import { cx } from "class-variance-authority";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 import { FaXTwitter } from "react-icons/fa6";
-import { useLocalStorage } from "usehooks-ts";
 import logoText from "./logo-text.svg";
-
-const Subscribe = () => {
-  const { t } = useLingui();
-  const [subscribed, setSubcribed] = useLocalStorage(
-    "subscribed_newsletter",
-    false,
-  );
-  const [loading, setLoading] = useState(false);
-  const [formState, { email }] = useFormState({
-    email: "",
-  });
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLoading(true);
-    await fetch("/api/newsletter", {
-      method: "POST",
-      body: JSON.stringify({
-        email: formState.email,
-        utmSource: "CanadaSpends",
-        utmMedium: "organic",
-        utmCampaign: "footer",
-      }),
-    });
-    setLoading(false);
-    setSubcribed(true);
-  };
-
-  return (
-    <div className="mt-10 xl:mt-0">
-      <h3 className="text-sm/6 font-semibold text-gray-900">
-        <Trans>Subscribe to our newsletter</Trans>
-      </h3>
-
-      {subscribed ? (
-        <p className="mt-2 text-sm/6 text-gray-600">
-          <Trans>Thank you for subscribing!</Trans>
-        </p>
-      ) : (
-        <>
-          <p className="mt-2 text-sm/6 text-gray-600">
-            <Trans>Get the facts weekly, right in your inbox.</Trans>
-          </p>
-          <form
-            className={cx("mt-6 sm:flex sm:max-w-md", loading && "opacity-50")}
-            onSubmit={onSubmit}
-          >
-            <label htmlFor="email-address" className="sr-only">
-              <Trans>Email address</Trans>
-            </label>
-            <input
-              disabled={loading}
-              {...email("email")}
-              required
-              placeholder={t`Enter your email`}
-              autoComplete="email"
-              className="w-full min-w-0 bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:w-64 sm:text-sm/6 xl:w-full"
-            />
-            <div className="mt-4 sm:ml-4 sm:mt-0 sm:shrink-0">
-              <button
-                disabled={loading}
-                type="submit"
-                className="flex w-full items-center justify-center bg-indigo-950 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                <Trans>Subscribe</Trans>
-              </button>
-            </div>
-          </form>
-        </>
-      )}
-    </div>
-  );
-};
 
 export const Footer = () => {
   return (
@@ -97,18 +20,19 @@ export const Footer = () => {
                   <ul role="list" className="mt-6 space-y-4">
                     <li>
                       <Link
-                        href="/spending"
+                        href="/about"
                         className="text-sm/6 text-gray-600 hover:text-gray-900"
                       >
-                        <Trans>Spending</Trans>
+                        <Trans>About Us</Trans>
                       </Link>
                     </li>
                     <li>
                       <Link
-                        href="/about"
+                        href="https://buildcanada.com/get-involved"
+                        target="_blank"
                         className="text-sm/6 text-gray-600 hover:text-gray-900"
                       >
-                        <Trans>About</Trans>
+                        <Trans>Get Involved</Trans>
                       </Link>
                     </li>
                     <li>
@@ -123,7 +47,21 @@ export const Footer = () => {
                 </div>
               </div>
             </div>
-            <Subscribe />
+            <div className="mt-10 xl:mt-0 flex flex-col gap-2">
+              <h3 className="text-sm/6 font-semibold text-gray-900">
+                <Trans>Subscribe to our newsletter</Trans>
+              </h3>
+              <p className="text-sm/6 text-gray-600">
+                <Trans>Get weekly recaps of current events and updates from our team.</Trans>
+              </p>
+              <a 
+                href="https://buildcanada.substack.com/subscribe"
+                target="_blank"
+                className="bg-indigo-950 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 rounded-md w-fit"
+              >
+                Subscribe
+              </a>
+            </div>
           </div>
 
           <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
